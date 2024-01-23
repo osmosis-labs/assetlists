29b99676f456c1800da0393e93c0a9fc5b771df4
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2024-01-23 10:50:22 -0700

    Convert Scripts to Use Generalized Functions (#1011)
    * update gen_zone_config to use assetlist_functions
    
    * update chain_reg_assetlist
    
    * update gen_upd_chain_reg_assetlist to use assetlist_functions

--- .github/workflows/utility/assetlist_functions.mjs
+++ .github/workflows/utility/assetlist_functions.mjs
@@ -107,8 +107,3 @@ export async function calculateIbcHash(ibcHashInput) {
   return ibcHashOutput;
 }
 
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}

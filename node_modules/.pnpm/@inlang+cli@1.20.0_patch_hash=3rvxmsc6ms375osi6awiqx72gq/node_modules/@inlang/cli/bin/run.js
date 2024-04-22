60b1958f12c92964d1921755304a7d3eb19b61ed
Jeremy Parish <jeremyparish69@gmail.com>
2024-04-21 21:57:22 -0600

    update node_modules

---
+++ node_modules/.pnpm/@inlang+cli@1.20.0_patch_hash=3rvxmsc6ms375osi6awiqx72gq/node_modules/@inlang/cli/bin/run.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// the comment above signals unix environments which interpreter
+// to use when executing this file (node)
+
+import { cli } from "../dist/main.js"
+
+try {
+	cli.parse()
+} catch (error) {
+	console.log("catch")
+	console.error(error)
+}

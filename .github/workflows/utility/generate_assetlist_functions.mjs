cf62db6e59264d1cd889d9377205a94974d38e0d
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2025-08-28 12:45:33 -0600

    Revert "Revert "Inherit all Assets from Cosmos Chain Registry (#2582)" (#2583)"
    This reverts commit e09013b3c0bd27f2284320647235c06b466e9de0.

--- .github/workflows/utility/generate_assetlist_functions.mjs
+++ .github/workflows/utility/generate_assetlist_functions.mjs
@@ -420,8 +420,8 @@ export function setIdentityAsset(asset_data) {
     ) { break; }
 
     let provider = null;
-    if ( traceTypesNeedingProvider.includes(traces[i].type) ) {
-      provider = asset_data.zone_config?.providers.find(
+    if (traceTypesNeedingProvider.includes(traces[i].type)) {
+      provider = asset_data.zone_config?.providers?.find(
         provider =>  //where...
           provider.provider === traces[i].provider
       )
@@ -1513,29 +1513,32 @@ export function setTransferMethods(asset_data) {
   });
 
   if (asset_data.source_asset.chain_name !== asset_data.chainName) {
-    const traces = getAssetProperty(asset_data.local_asset, "traces");
-    const trace = traces?.[traces.length - 1];
-    const ibcTransferMethod = {
-      name: "Osmosis IBC Transfer",
-      type: "ibc",
-      counterparty: {
-        chainName: trace.counterparty.chain_name,
-        chainId: chain_reg.getFileProperty(
-          trace.counterparty.chain_name,
-          "chain",
-          "chain_id"
-        ),
-        sourceDenom: trace.counterparty.base_denom,
-        port: trace.counterparty.port ?? "transfer",
-        channelId: trace.counterparty.channel_id
-      },
-      chain: {
-        port: trace.chain.port ?? "transfer",
-        channelId: trace.chain.channel_id,
-        path: trace.chain.path
+    const live = chain_reg.getFileProperty(asset_data.source_asset.chain_name, "chain", "status");
+    if (live === "live") {
+      const traces = getAssetProperty(asset_data.local_asset, "traces");
+      const trace = traces?.[traces.length - 1];
+      const ibcTransferMethod = {
+        name: "Osmosis IBC Transfer",
+        type: "ibc",
+        counterparty: {
+          chainName: trace.counterparty.chain_name,
+          chainId: chain_reg.getFileProperty(
+            trace.counterparty.chain_name,
+            "chain",
+            "chain_id"
+          ),
+          sourceDenom: trace.counterparty.base_denom,
+          port: trace.counterparty.port ?? "transfer",
+          channelId: trace.counterparty.channel_id
+        },
+        chain: {
+          port: trace.chain.port ?? "transfer",
+          channelId: trace.chain.channel_id,
+          path: trace.chain.path
+        }
       }
+      transferMethods.push(ibcTransferMethod);
     }
-    transferMethods.push(ibcTransferMethod);
   }
 
   asset_data.frontend.transferMethods = transferMethods;
